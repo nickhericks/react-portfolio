524cb30945668647f9de9830f8e612f1ca97deda
Nick-Hericks <nickhericks@gmail.com>
2019-08-23 09:14:56 +0200

    Add google fonts

--- src/App.js
+++ src/App.js
@@ -4,55 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-
+      <header className="App-header">
         <div>
-          <span className="wave" role='img' aria-label='hand wave emoji'>
-						👋
-					</span>
+          <span className="wave" role="img" aria-label="hand wave emoji">
+            👋
+          </span>
         </div>
 
         <p>
-					<h1 className="name">
-						Hello, my name is <br class="mobile-break"></br><span className="handwriting">Nick Hericks</span>
-					</h1>
-					<h2 className="role">I'm a full stack developer with a passion for building tools and communities on the web.</h2>
+          <h1 className="name">
+            Hello, my name is <br class="mobile-break" />
+            <span className="handwriting">Nick Hericks</span>
+          </h1>
+          <h2 className="role">
+            I'm a full stack developer with a passion for building tools and
+            communities on the web.
+          </h2>
         </p>
+      </header>
 
-
-
-
-      </header> */}
-
-			
-			<section className='timeline'>
+      {/* <section className='timeline'>
 				<div className='left-path'>
-
 				</div>
 				<div className='right-path'>
-
 				</div>
+			</section> */}
 
+      <section className="highlights">
+        <h1>Client highlights</h1>
+        <img
+          className="card"
+          src="https://res.cloudinary.com/dtqevfsxh/image/upload/v1543327352/portfolio/pledge_screenshot_1200.png"
+          alt="pledge health project"
+        />
+        <h3 class="project-name">Pledge Health</h3>
+        <h4 class="link-name">pledgehealth.org</h4>
+				<p class="testimonial">
+					"...redesign of our website catalyzed our nonprofit to a new phase of growth which included fantastic fundraising success, which I attribute in large part to how proud we were to share our beautiful new site."
+					<br></br><br></br><span class="client-handwriting">
+					- Abigail Hankin-Wei, MD, MPH,<br></br>Vice President, PLeDGE Health</span>
+        </p>
+      </section>
 
-
-
-			</section>
-
-
-			<section className='highlights'>
-
-			</section>
-
-			<section className='projects'>
-
-			</section>
-
-
-
-
-
-
-
+      <section className="projects" />
     </div>
   );
 }
